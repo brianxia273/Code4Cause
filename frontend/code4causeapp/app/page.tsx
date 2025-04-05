52356089886cc00104fbaeca3c369482c34fe8fb
brianxia273 <briangxia@gmail.com>
2025-04-05 11:57:26 -0400

    f

---
+++ frontend/code4causeapp/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Container } from "@/components/layout/Container";
+
+export default function Home() {
+  return (
+    <div>
+      <Container></Container>
+    </div>
+  );
+}
